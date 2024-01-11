29f2eea86c5f82e183b9289496b4f0daf36ca5f0
mahi8221 <152174161+mahi8221@users.noreply.github.com>
2024-01-11 10:04:58 +0530

    Add files via upload

---
+++ Vaccination/src/main/java/com/ecommerce/service/VaccineCenterService.java
@@ -0,0 +1,16 @@
+package com.ecommerce.service;
+import java.util.List;
+import com.ecommerce.entity.VaccineCenter;
+
+public interface VaccineCenterService {
+
+VaccineCenter addVaccineCenter(VaccineCenter vaccineCenter);
+
+void updateVaccineCenter(VaccineCenter updatedVaccineCenter);
+
+void deleteVaccineCenter(Long id);
+
+VaccineCenter getVaccineCenterById(Long id);
+
+List<VaccineCenter> getAllVaccineCenters();
+} 
